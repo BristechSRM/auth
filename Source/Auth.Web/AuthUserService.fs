d4dfc3c598fa6100f0ba763aa032ae43bc833fb0
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-04-20 16:26:18 +0100

    Add connection and basic getAll and add user service methods

---
+++ Source/Auth.Web/AuthUserService.fs
@@ -0,0 +1,30 @@
+﻿module AuthUserService
+
+open System
+open Amazon
+open Amazon.DynamoDBv2
+open Amazon.DynamoDBv2.DataModel
+open Amazon.DynamoDBv2.DocumentModel
+open Serilog
+open Entities
+
+let client = new AmazonDynamoDBClient(RegionEndpoint.EUWest1)
+let context = new DynamoDBContext(client)
+
+let getUsers() = 
+    Log.Information("Accessing DynamoDb for AuthUsers")
+    context.Scan<AuthUserEntity>()
+
+let createUser firstName surName email = 
+    let user = 
+        { Id = Guid.NewGuid()
+          Firstname = firstName
+          Surname = surName
+          Email = email }
+    
+    let userBatch = context.CreateBatchWrite<AuthUserEntity>()
+    userBatch.AddPutItem(user)
+    Log.Information("Executing user batch write")
+    userBatch.Execute()
+    let newUser = context.Load<AuthUserEntity>(user.Id)
+    newUser
---
+++ Source/Auth.Web/Entities.fs
@@ -0,0 +1,12 @@
+﻿module Entities
+
+open System
+open Amazon.DynamoDBv2.DataModel
+
+[<DynamoDBTable("Auth.Users")>]
+[<CLIMutable>]
+type AuthUserEntity = 
+    { Id : Guid
+      Email : string
+      Firstname : string
+      Surname : string }
