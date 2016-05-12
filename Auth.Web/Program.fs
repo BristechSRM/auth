545efbe0ff64ca8279d8a702747ab6dd89b9cef4
Paul Allen <pallen@scottlogic.co.uk>
2016-05-12 15:59:25 +0100

    Put BaseUrl in config

--- Auth.Web/Program.fs
+++ Auth.Web/Program.fs
@@ -1,20 +1,37 @@
 module Program
 
 open Microsoft.Owin.Hosting
+open System
+open System.Configuration
 open System.Threading
-open Startup
 open Logging
 open Serilog
+open Startup
 
+(*
+    Do not run Visual Studio as Administrator!
+
+    Open a command prompt as Administrator and run the following command, replacing username with your username
+    netsh http add urlacl url=http://*:8080/ user=username
+*)
 [<EntryPoint>]
 let main _ =
     setupLogging()
 
-    let baseAddress = "http://*:9003"
-    use server = WebApp.Start<Startup>(baseAddress)
-    Log.Information("Listening on {Address}", baseAddress)
+    try
+        let baseUrl = ConfigurationManager.AppSettings.Get("BaseUrl")
+        if String.IsNullOrEmpty baseUrl then
+            failwith "Missing configuration value: 'BaseUrl'"
 
-    let waitIndefinitelyWithToken =
-        let cancelSource = new CancellationTokenSource()
-        cancelSource.Token.WaitHandle.WaitOne() |> ignore
-    0
+        use server = WebApp.Start<Startup>(baseUrl)
+        Log.Information("Listening on {Address}", baseUrl)
+
+        let waitIndefinitelyWithToken = 
+            let cancelSource = new CancellationTokenSource()
+            cancelSource.Token.WaitHandle.WaitOne() |> ignore
+        0
+
+    with
+    | ex ->
+        Log.Fatal("Exception: {0}", ex)
+        -1
